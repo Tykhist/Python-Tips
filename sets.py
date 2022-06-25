dd0c97deda757518a41c12f45f8093161d87709c
William Smith <mr.williamjdsmith@gmail.com>
2022-06-24 23:59:48 -0700

    Create sets.py

---
+++ sets.py
@@ -0,0 +1,7 @@
+# Tip: Store unique values with Sets
+
+example = sorted([i for i in "Tip: Store unique values with Sets"])
+print(example, "\n")
+
+example_set = sorted(set(example))
+print(example_set)
